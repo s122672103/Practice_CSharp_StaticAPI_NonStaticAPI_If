369cd0321d136218565f853444ee42b4f2d64c45
OLIVER  HSU <s122672103@gmail.com>
2019-11-26 13:02:25 +0800

    玩家、殭屍產生隨機攻擊

---
+++ UnityProject/Assets/Script/Player.cs
@@ -0,0 +1,73 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [Header("血量")]
+    public float Hp = 50;
+    [Header("攻擊力")]
+    public float Atk = 0;
+    [Header("殭屍")]
+    public Zombie Zb;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+
+
+    private void Update()
+    {
+
+        PlayerAtk();
+
+
+    }
+
+
+    public void PlayerAtk()
+    {
+
+
+        if (Zb.Hp <= 0 || Hp <= 0)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Atk =  Random.Range(10f, 20f);
+            Zb.Hp = Zb.Hp - Atk;
+            
+            print("殭屍受到傷害:" + Atk);
+            print("殭屍剩下血量:" + Zb.Hp);
+            Zb.ZombieHurt();
+            Aud.PlayOneShot(SoundAtk, 1f);
+        }
+
+
+
+    }
+
+    public void PlayerHurt()
+    {
+
+        if (Hp <= 0)
+        {
+            PlayerDie();
+
+        }
+
+    }
+
+    public void PlayerDie()
+    {
+      
+        
+       print("玩家死亡");
+        
+
+    }
+
+
+}
---
+++ UnityProject/Assets/Script/Zombie.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+
+    [Header("血量")]
+    public float Hp = 50;
+    [Header("攻擊力")]
+    public float Atk = 0 ;
+    [Header("玩家")]
+    public Player Player;
+    [Header("喇叭")]
+    public AudioSource Aud;
+    [Header("音效")]
+    public AudioClip SoundAtk;
+
+    private void Update()
+    {
+
+        ZombieAtk();
+        
+
+
+    }
+
+
+    public void ZombieAtk()
+    {
+
+        if (Player.Hp <= 0 || Hp <= 0)
+        {
+            return;
+        }
+  
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Atk =  Random.Range(10f, 20f);
+            Player.Hp = Player.Hp - Atk;
+            print("玩家受到傷害:" + Atk);
+            print("玩家剩下血量:" + Player.Hp);
+            Player.PlayerHurt();
+            Aud.PlayOneShot(SoundAtk, 1f);
+        }
+
+    }
+
+    public void ZombieHurt()
+    {
+        if (Hp <= 0)
+        {
+            ZombieDie();
+
+        }
+         
+    }
+
+    public void ZombieDie()
+    {
+        print("殭屍死亡");
+
+    }
+
+
+
+}
